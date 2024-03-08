c8c3d9e10102295273f9d87c22d90073e1e1688d
athach <alcthach@gmail.com>
2024-03-07 22:41:04 -0500

    normalize occupancy_date field across all stgoccupancy models, WIP: profile location_id, investigate duplicate across all years

--- dbt_toronto_daily_shelter_overnight_services/models/staging/tdsos/stg_daily_shelter_occupancy_2017.sql
+++ dbt_toronto_daily_shelter_overnight_services/models/staging/tdsos/stg_daily_shelter_occupancy_2017.sql
@@ -8,7 +8,19 @@ source as (
 
 staged as (
 
-	select *
+	select 	_id,
+			DATE(occupancy_date) as occupancy_date,
+			organization_name,
+			shelter_name,
+			shelter_address,
+			shelter_city,
+			shelter_province,
+			shelter_postal_code,
+			facility_name,
+			program_name,
+			sector,
+			occupancy,
+			capacity
 	from source
 
 )
--- dbt_toronto_daily_shelter_overnight_services/models/staging/tdsos/stg_daily_shelter_overnight_service_occupancy_2021.sql
+++ dbt_toronto_daily_shelter_overnight_services/models/staging/tdsos/stg_daily_shelter_overnight_service_occupancy_2021.sql
@@ -8,7 +8,39 @@ source as (
 
 staged as (
 
-	select *
+	select	_id,
+			to_date( concat('20', occupancy_date), 'YYYY-MM-DD' ) as occupancy_date,
+			organization_id,
+			organization_name,
+			shelter_id,
+			shelter_group,
+			location_id,
+			location_name,
+			location_address,
+			location_postal_code,
+			location_city,
+			location_province,
+			program_id,
+			program_name,
+			sector,
+			program_model,
+			overnight_service_type,
+			program_area,
+			service_user_count,
+			capacity_type,
+			capacity_actual_bed,
+			capacity_funding_bed,
+			occupied_beds,
+			unoccupied_beds,
+			unavailable_beds,
+			capacity_actual_room,
+			capacity_funding_room,
+			occupied_rooms,
+			unoccupied_rooms,
+			unavailable_rooms,
+			occupancy_rate_beds,
+			occupancy_rate_rooms
+
 	from source
 
 )
